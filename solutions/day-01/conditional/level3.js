d8b44851f769bc204a0baae2844a93335500debd
Tomas Dal Verme <tdalverme@hotmail.com>
2022-08-06 14:54:16 -0300

    'Conditionals' completed

--- solutions/day-01/conditional/level3.js
+++ solutions/day-01/conditional/level3.js
@@ -8,3 +8,19 @@ if (['january', 'march', 'may', 'august', 'october', 'december'].includes(month.
 } else {
     console.log(month, 'has 30 days.')
 }
+
+// 2. Write a program which tells the number of days in a month, now consider leap year.
+month = prompt('Enter a month:')
+let year = prompt('Enter a year:')
+
+if (['january', 'march', 'may', 'august', 'october', 'december'].includes(month.toLowerCase())) {
+    console.log(month, 'has 31 days.')
+} else if(month === 'february') {
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+        console.log(month, 'has 29 days.')
+    } else {
+        console.log(month, 'has 28 days.')
+    }
+} else {
+    console.log(month, 'has 30 days.')
+}
